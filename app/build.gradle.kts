023f1a79fea2b0fefaad9d301d2482827670b400
Aleks <erldimitrovgrad@gmail.com>
2024-05-17 17:35:58 +0300

    change build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,11 +17,11 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
-    implementation ("org.json:json:20200518")
+    implementation ("org.json:json:20231013")
     testImplementation("org.assertj:assertj-core:3.24.2")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
+    implementation(dependencyNotation = "com.fasterxml.jackson.core:jackson-databind:2.14.2")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.14.2")
     implementation("info.picocli:picocli:4.7.5")
     annotationProcessor("info.picocli:picocli-codegen:4.7.5")
