a0ceb2ee75c98a2653086b267588e07b80468d56
Aleks <erldimitrovgrad@gmail.com>
2024-05-17 23:37:30 +0300

    change README.md

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -28,11 +28,11 @@ public class Differ {
         return Formatter.format(format, diff);
     }
     //Метод для прохождения тестов хекслет
-    public static String generate(String filepath1, String filepath2,String format) throws IOException {
+    public static String generate(String filepath1, String filepath2, String format2) throws IOException {
         Map<String, Object> map1 = Parser.readFile(filepath1);
         Map<String, Object> map2 = Parser.readFile(filepath2);
         Map<String, Object[]> diff = compareMaps(map1, map2);
-        return Formatter.format(format, diff);
+        return Formatter.format(format2, diff);
     }
      /**
      * Сравнивает два Map и возвращает Map с различиями.
--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -8,11 +8,6 @@ import hexlet.code.formatters.Stylish;
 import java.util.Map;
 
 public class Formatter {
-    private String format;
-
-    public Formatter(String format) {
-        this.format = format;
-    }
 
     public static String format(String format, Map<String, Object[]> diff) throws JsonProcessingException {
         switch (format) {
