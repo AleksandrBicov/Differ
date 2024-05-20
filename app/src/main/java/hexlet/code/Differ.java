afb148ab26decfbab5353ca0c301f26785aea1b9
Aleks <erldimitrovgrad@gmail.com>
2024-05-20 21:36:24 +0300

    change App.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -16,20 +16,15 @@ public class Differ {
     /**
      * Генерирует отчет о различиях между двумя файлами.
      *
-     * @param filepath1 Путь к первому файлу.
-     * @param filepath2 Путь ко второму файлу.
+     * @param filePath1 Путь к первому файлу.
+     * @param filePath2 Путь ко второму файлу.
      * @return Отчет о различиях в виде строки.
      * @throws IOException Если возникает ошибка при чтении файлов.
      */
-    public static String generate(String filepath1, String filepath2) throws IOException {
-        Map<String, Object> map1 = Parser.readFile(filepath1);
-        Map<String, Object> map2 = Parser.readFile(filepath2);
-        Formatter.notNull(map1);
-        Formatter.notNull(map2);
-        Map<String, Object[]> diff = compareMaps(map1, map2);
-        return Formatter.format(format, diff);
+    public static String generate(String filePath1, String filePath2) throws IOException {
+        return generate(filePath1, filePath2, format);
     }
-    //Метод для прохождения тестов хекслет
+
     public static String generate(String filepath1, String filepath2, String format2) throws IOException {
         Map<String, Object> map1 = Parser.readFile(filepath1);
         Map<String, Object> map2 = Parser.readFile(filepath2);
--- app/src/main/java/hexlet/code/formatters/JsonFormatter.java
+++ app/src/main/java/hexlet/code/formatters/JsonFormatter.java
@@ -3,44 +3,11 @@ package hexlet.code.formatters;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class JsonFormatter {
     public static String format(Map<String, Object[]> diff) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
-        List<Map<String, Object>> formattedDiff = new ArrayList<>();
-
-        for (Map.Entry<String, Object[]> entry : diff.entrySet()) {
-            String key = entry.getKey();
-            Object[] values = entry.getValue();
-            Object oldValue = values[0];
-            Object newValue = values[1];
-            String status = getStatus(oldValue, newValue);
-
-            Map<String, Object> change = new HashMap<>();
-            change.put("key", key);
-            change.put("oldvalue", oldValue);
-            change.put("newvalue", newValue);
-            change.put("status", status);
-
-            formattedDiff.add(change);
-        }
-
-        return mapper.writeValueAsString(formattedDiff);
-    }
-
-    private static String getStatus(Object oldValue, Object newValue) {
-        if (oldValue == null && newValue != null) {
-            return "added";
-        } else if (oldValue != null && newValue == null) {
-            return "removed";
-        } else if (!oldValue.equals(newValue)) {
-            return "updated";
-        } else {
-            return "unmodified";
-        }
+        return mapper.writeValueAsString(diff);
     }
 }
